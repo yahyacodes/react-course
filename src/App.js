95ee32c602185a753d1ba3d9d2492c20a3f740df
yahya.codin <yahya.codin@gmail.com>
2022-08-21 18:01:27 +0300

    m

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get Started!</h2>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
