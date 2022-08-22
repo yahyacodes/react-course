56b0308026339cf714eb606a6d4b08ce5433eb37
yahyacodes <yahya.codin@gmail.com>
2022-08-22 12:29:40 +0300

    m

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
 const App = () => {
@@ -24,6 +25,11 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = (expense) => {
+    console.log("In App.js");
+    console.log(expense);
+  };
+
   // return React.createElement(
   //   "div",
   //   {},
@@ -33,7 +39,7 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get Started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
 
       <Expenses items={expenses} />
     </div>
